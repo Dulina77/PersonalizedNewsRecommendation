90364f18b4e8a5cded6675035033a140f7dc38d2
Dulina77 <dulinawickramasinghe@gmail.com>
2024-11-24 00:44:53 +0530

    update username and email validation in the RegisterPageController class.

--- src/main/java/com/example/cw1/MainApplication.java
+++ src/main/java/com/example/cw1/MainApplication.java
@@ -13,7 +13,7 @@ public class MainApplication extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
             Scene scene = new Scene(root);
-            stage.setTitle("Your News Recommendation Application");
+            stage.setTitle("Your News Recommendation Applicaton");
             stage.setScene(scene); // Set the window size
             stage.show();
         } catch (Exception e) {
